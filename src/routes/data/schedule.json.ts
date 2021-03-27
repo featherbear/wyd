65aec1cc36135c02460622dcf6559d191f53c92b
Andrew Wong <andrew.j.wong@outlook.com>
2021-03-28 01:27:20 +1100

    Sort result

--- src/routes/data/schedule.json.ts
+++ src/routes/data/schedule.json.ts
@@ -1,14 +1,19 @@
 import ICSManager from "../../components/ICS";
 
 export async function get(req, res, next) {
-  let data = await ICSManager.data;
+  let data = (await ICSManager.data).flat();
 
   // Strip sensitive data
-  data = data.map(({ dtstart, dtend }) => ({
+  let cleaned = data.map(({ dtstart, dtend }) => ({
     dtstart,
     dtend,
   }));
 
+  let sorted = cleaned.sort(
+    ({ dtstart: a }, { dtstart: b }) =>
+      new Date(a).getTime() - new Date(b).getTime()
+  );
+
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data, null, 4));
+  res.end(JSON.stringify(sorted, null, 4));
 }
